556451cf2c3b4188c321d026ba7ad010a7dd4145
Reza Lotfi <reza.lotfi14@gmail.com>
2019-04-11 14:05:28 -0700

    Remove require path and console.log

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const axios = require('axios');
 const app = express();
 const port = 9000;
@@ -8,7 +7,6 @@ app.use('/hostels/:Id', express.static('public'));
 
 app.get('/api/hostels/:id', (req, res) => {
   const Id = req.params.id;
-  console.log(Id)//works
   axios.get(`http://localhost:3001/api/hostels/${Id}`)
     .then(response => (res.status(200).send(response.data)))
     .catch(err => console.log(err))
