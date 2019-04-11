956c119c7d098b3cc367a8ad42f9f89e34755fcc
Reza Lotfi <reza.lotfi14@gmail.com>
2019-04-11 12:26:17 -0700

    Implement proxy server routes

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+const app = express();
+const port = 9000;
+
+app.use('/hostels/:Id', express.static('public'));
+
+app.get('/api/hostels/:id', (req, res) => {
+  const Id = req.params.id;
+  console.log(Id)//works
+  axios.get(`http://localhost:3001/api/hostels/${Id}`)
+    .then(response => (res.status(200).send(response.data)))
+    .catch(err => console.log(err))
+})
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
